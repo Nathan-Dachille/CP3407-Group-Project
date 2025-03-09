63ee6c2448685a273df1e1a901fbea1a2485e692
Dav0-12 <140329260+Dav0-12@users.noreply.github.com>
2025-03-09 12:59:37 +1000

    Delete necessary forms.py file

--- src/pages/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class SignIn(forms.Form):
-    username = forms.EmailField(label="Email", max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput, label="Password", max_length=100)
