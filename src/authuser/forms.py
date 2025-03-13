5a21c1c4f2140677b20221363b3ef91d3cee17fa
Dav0-12 <140329260+Dav0-12@users.noreply.github.com>
2025-03-13 13:05:04 +1000

    Create a basic custom registration form that is working

---
+++ src/authuser/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+
+from authuser.models import User
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=255, help_text="Required. Add a valid email address.")
+
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        try:
+            user = User.objects.get(email=email)
+        except Exception as e:
+            return email
+        raise forms.ValidationError(f"Email  {email} is already in use.")
