0c793f94edaaf160512cee99d7e9f5fa7777acce
Quinn-Stocker <quinn.stocker@my.jcu.edu.au>
2025-03-13 23:09:34 +1000

    linked page to the rest of the website through the button on navbar

--- src/account/views.py
+++ src/account/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-# @login_required
+@login_required(login_url="/sign_in/")
 def account(request, *args, **kwargs):
     return render(request, "account.html", {})
