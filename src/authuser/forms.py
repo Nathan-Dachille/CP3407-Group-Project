ef2bd71e7ec7b9776008efcd7117116462e3a933
Dav0-12 <140329260+Dav0-12@users.noreply.github.com>
2025-03-13 21:59:31 +1000

    Removed the need for a different email address per account

--- src/authuser/forms.py
+++ src/authuser/forms.py
@@ -11,14 +11,6 @@ class RegistrationForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'phone', 'first_name', 'last_name', 'role', 'password1', 'password2')
 
-    def clean_email(self):
-        email = self.cleaned_data['email'].lower()
-        try:
-            user = User.objects.get(email=email)
-        except Exception as e:
-            return email
-        raise forms.ValidationError(f"Email  {email} is already in use.")
-
 
 class SignInForm(AuthenticationForm):
     email = forms.EmailField(max_length=255)
