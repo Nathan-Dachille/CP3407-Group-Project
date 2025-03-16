8b14db0fd8c063615bd15aea0ef81fd91488196d
Dav0-12 <140329260+Dav0-12@users.noreply.github.com>
2025-03-17 01:43:12 +1000

    Added ratings field in the custom user model

--- src/authuser/models.py
+++ src/authuser/models.py
@@ -66,3 +66,4 @@ class User(AbstractUser):
                              validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                                         message="Phone number must be entered in the format: "
                                                                 "'+999999999'. Up to 15 digits allowed.")])
+    rating = models.IntegerField(null=True)
