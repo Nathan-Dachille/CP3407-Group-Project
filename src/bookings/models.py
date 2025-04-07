f9324876584d30ae1a7fd8d029f0c78533eeae6c
Quinn-Stocker <quinn.stocker@my.jcu.edu.au>
2025-04-08 02:32:58 +1000

    Added automatic removal of old unassigned booking objects, activates whenever object is called

--- src/bookings/models.py
+++ src/bookings/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 from authuser.models import User
+from django.utils import timezone
+
+
+class BookingQuerySet(models.QuerySet):
+    def validate_and_clean(self):
+        for booking in self:
+            booking.delete_if_old_and_unassigned()
+        return self
+
+
+class BookingManager(models.Manager):
+    def get_queryset(self):
+        return BookingQuerySet(self.model, using=self._db).validate_and_clean()
 
 
 class Booking(models.Model):
@@ -13,6 +26,22 @@ class Booking(models.Model):
     assigned = models.ForeignKey(User, related_name='assigned_bookings', blank=True, null=True, on_delete=models.SET_NULL)
     cleaner_rating = models.IntegerField(null=True)
     customer_rating = models.IntegerField(null=True)
+
+    objects = BookingManager()  # Attach your manager here
+
+    def should_be_deleted(self):
+        # Check if the booking is unassigned and old
+        now = timezone.now()
+        if self.assigned is None and self.date < now.date():
+            if self.date == now.date() and self.start_time < now.time():
+                return True
+            elif self.date < now.date():
+                return True
+        return False
+
+    def delete_if_old_and_unassigned(self):
+        if self.should_be_deleted():
+            self.delete()
 
     def __str__(self):
         return f"Booking by {self.user.email} on {self.date} from {self.start_time} to {self.end_time or 'N/A'}"
